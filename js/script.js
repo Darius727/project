b0d44561c8716a660634a37cfef05de4e1307924
Oleg <example@gmail.com>
2023-04-10 15:07:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// alert('hello');
+// const resault = confirm('Are you here?');
+
+// console.log(resault);
+// console.log('confirm');
+
+// const answer = prompt("what your age?", "34");
+// console.log(answer)
+
+let answers = [];
+
+answers[0] = prompt('whar your name?', '');
+answers[1] = prompt('how ald are you?', '');
+answers[2] = prompt('what your secondname?', '');
+console.log(answers)
